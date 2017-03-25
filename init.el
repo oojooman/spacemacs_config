66a7f0bcc784f902c49b166eb27f0fb32ba63560
Oojooman <steiner.mischa@gmail.com>
2017-03-25 23:44:54 +0900

    initial commit

---
+++ init.el
@@ -0,0 +1,327 @@
+;; -*- mode: emacs-lisp -*-
+;; This file is loaded by Spacemacs at startup.
+;; It must be stored in your home directory.
+
+(defun dotspacemacs/layers ()
+  (setq-default
+   dotspacemacs-distribution 'spacemacs
+   dotspacemacs-enable-lazy-installation 'unused
+   dotspacemacs-ask-for-lazy-installation t
+   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layers
+   '(
+     csv
+     javascript
+     html
+     elixir
+     helm
+     auto-completion
+     ;; better-defaults
+     emacs-lisp
+     git
+     markdown
+     org
+     ;; (shell :variables
+     ;;        shell-default-height 30
+     ;;        shell-default-position 'bottom)
+     ;; spell-checking
+     syntax-checking
+     ;; version-control
+     gnus
+     finance
+     )
+   dotspacemacs-additional-packages '()
+   dotspacemacs-frozen-packages '()
+   dotspacemacs-excluded-packages '()
+   dotspacemacs-install-packages 'used-only))
+
+(defun dotspacemacs/init ()
+  "Initialization function.
+This function is called at the very startup of Spacemacs initialization
+before layers configuration.
+You should not put any user code in there besides modifying the variable
+values."
+  (setq-default
+   dotspacemacs-elpa-https t
+   dotspacemacs-elpa-timeout 5
+   dotspacemacs-check-for-update nil
+   dotspacemacs-elpa-subdirectory nil
+   dotspacemacs-editing-style 'vim
+   dotspacemacs-verbose-loading nil
+   dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-lists '((recents . 5)
+                                (projects . 7))
+   dotspacemacs-startup-buffer-responsive t
+   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-themes '(spacemacs-dark
+                         spacemacs-light)
+   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-default-font '("Ubuntu Mono"
+                               :size 13
+                               :weight normal
+                               :width normal
+                               :powerline-scale 1.0)
+   dotspacemacs-leader-key "SPC"
+   dotspacemacs-emacs-command-key "SPC"
+   dotspacemacs-ex-command-key ":"
+   dotspacemacs-emacs-leader-key "M-m"
+   dotspacemacs-major-mode-leader-key ","
+   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   dotspacemacs-distinguish-gui-tab nil
+   dotspacemacs-remap-Y-to-y$ t
+   dotspacemacs-retain-visual-state-on-shift t
+   dotspacemacs-visual-line-move-text nil
+   dotspacemacs-ex-substitute-global nil
+   dotspacemacs-default-layout-name "Default"
+   dotspacemacs-display-default-layout nil
+   dotspacemacs-auto-resume-layouts nil
+   dotspacemacs-large-file-size 1
+   dotspacemacs-auto-save-file-location 'cache
+   dotspacemacs-max-rollback-slots 5
+   dotspacemacs-helm-resize nil
+   dotspacemacs-helm-no-header nil
+   dotspacemacs-helm-position 'bottom
+   dotspacemacs-helm-use-fuzzy 'always
+   dotspacemacs-enable-paste-transient-state nil
+   dotspacemacs-which-key-delay 0.4
+   dotspacemacs-which-key-position 'bottom
+   dotspacemacs-loading-progress-bar t
+   dotspacemacs-fullscreen-at-startup t
+   dotspacemacs-fullscreen-use-non-native nil
+   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-active-transparency 90
+   dotspacemacs-inactive-transparency 90
+   dotspacemacs-show-transient-state-title t
+   dotspacemacs-show-transient-state-color-guide t
+   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-smooth-scrolling t
+   dotspacemacs-line-numbers nil
+   dotspacemacs-folding-method 'evil
+   dotspacemacs-smartparens-strict-mode nil
+   dotspacemacs-smart-closing-parenthesis nil
+   dotspacemacs-highlight-delimiters 'all
+   dotspacemacs-persistent-server nil
+   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   dotspacemacs-default-package-repository nil
+   dotspacemacs-whitespace-cleanup nil
+   ))
+
+(defun dotspacemacs/user-init ()
+  )
+
+(defun my-copy-simple (&optional beg end)
+  "Save the current region (or line) to the `kill-ring' after stripping extra whitespace and new lines"
+  (interactive
+   (if (region-active-p)
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-end-position))))
+  (let ((my-text (buffer-substring-no-properties beg end)))
+    (with-temp-buffer
+      (insert my-text)
+      (goto-char 1)
+      (while (looking-at "[ \t\n]")
+        (delete-char 1))
+      (let ((fill-column 9333999))
+        (fill-region (point-min) (point-max)))
+      (kill-region (point-min) (point-max)))))
+
+(defun compress-org-link (arg)
+  (interactive "P")
+  (let ((url (thing-at-point 'url))
+        (bounds (bounds-of-thing-at-point 'url)))
+    (kill-region (car bounds) (cdr bounds))
+    (insert (format "[[%s][%s]]" url (truncate-string-to-width url (if arg (prefix-numeric-value arg) 32) nil nil "...")))))
+
+(defun my/copy-id-to-clipboard() "Copy the ID property value to killring, if no ID is there then create a new unique ID. This function works only in org-mode buffers.
+The purpose of this function is to easily construct id:-links to org-mode items. If its assigned to a key it saves you marking the text and copying to the killring."
+       (interactive)
+       (when (eq major-mode 'org-mode) ; do this only in org-mode buffers
+         (setq mytmpid (funcall 'org-id-get-create))
+         (kill-new mytmpid)
+         (message "Copied %s to killring (clipboard)" mytmpid)
+         ))
+
+;; (global-set-key (kbd "") 'my/copy-id-to-clipboard)
+
+(defun xftp (&optional frame)
+  "Return t if FRAME support XFT font backend."
+  (let ((xft-supported))
+    (mapc (lambda (x) (if (eq x 'xft) (setq xft-supported t)))
+          (frame-parameter frame 'font-backend))
+    xft-supported))
+
+(when (xftp)
+  (let ((fontset "fontset-default"))
+    (set-fontset-font fontset 'latin
+                      '("DejaVu Sans Mono" . "unicode-bmp"))
+    (set-fontset-font fontset 'hangul
+                      '("NanumGothic" . "unicode-bmp"))
+    (set-face-attribute 'default nil
+                        :font fontset
+                        :height 110)))
+
+(defun dotspacemacs/user-config ()
+
+  (global-set-key (kbd "<XF86HomePage>") 'keyboard-quit)
+
+  (global-set-key (kbd "C-c M-u") (lambda () (interactive) (insert "ü")))
+  (global-set-key (kbd "C-c M-a") (lambda () (interactive) (insert "ä")))
+  (global-set-key (kbd "C-c M-o") (lambda () (interactive) (insert "ö")))
+
+  ;; Get email, and store in nnml
+  (setq gnus-secondary-select-methods
+        '(
+          (nnimap "gmail"
+                  (nnimap-address
+                   "imap.gmail.com")
+                  (nnimap-server-port 993)
+                  (nnimap-stream ssl))
+          ))
+
+  ;; Send email via Gmail:
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-default-smtp-server "smtp.gmail.com")
+
+  ;; Archive outgoing email in Sent folder on imap.gmail.com:
+  (setq gnus-message-archive-method '(nnimap "imap.gmail.com")
+        gnus-message-archive-group "[Gmail]/Sent Mail")
+
+  ;; set return email address based on incoming email address
+  (setq gnus-posting-styles
+        '(((header "to" "address@outlook.com")
+           (address "address@outlook.com"))
+          ((header "to" "address@gmail.com")
+           (address "address@gmail.com"))))
+
+  ;; store email in ~/gmail directory
+  (setq nnml-directory "~/gmail")
+  (setq message-directory "~/gmail")
+
+  ;; Display time and load in mode line
+  (display-time-mode 1)
+  ;; Powerline settings
+  (setq powerline-default-separator 'box)
+  (setq ns-use-srgb-colorspace nil)
+  (spaceline-compile)
+
+  ;; web-mode settings
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+
+  ;; org-mode settings
+
+  ;; Standard key bindings
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
+
+  ;; (setq org-agenda-files (list "~/Dropbox/org/"))
+
+  (setq visual-line-mode t)
+  (setq org-indent-mode t)
+
+  (setq org-todo-keywords
+        (quote ((sequence "NEXT(n)" "TODO(t)" "|" "DONE(d)")
+                (sequence "WAIT(w)" "HOLD(h)" "|" "MEMO(m)" "CANCELLED(c)"))))
+
+  (setq org-default-notes-file "~/Dropbox/org/refile.org")
+
+  ;; Targets include this file and any file contributing to the agenda
+  ;; - up to 9 levels deep
+  (setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                   (org-agenda-files :maxlevel . 9))))
+
+  ;; Use full outline paths for refile targets - we file directly with IDO
+  (setq org-refile-use-outline-path t)
+
+  ;; Targets complete directly with IDO
+  (setq org-outline-path-complete-in-steps nil)
+
+  ;; Allow refile to create parent tasks with confirmation
+  (setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+  (setq org-startup-indented t)
+  (setq org-cycle-separator-lines 0)
+  (setq org-reverse-note-order t)
+
+  ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+  (setq org-capture-templates
+        (quote (("t" "todo" entry (file "~/Dropbox/org/refile.org")
+                 "* TODO %?\n%U\n%a\n")
+                ("r" "respond" entry (file "~/Dropbox/org/refile.org")
+                 "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+                ("n" "note" entry (file "~/Dropbox/org/refile.org")
+                 "* %? :NOTE:\n%U\n%a\n")
+                ("j" "Journal" entry (file "~/Dropbox/org/refile.org")
+                 "* %U %?")
+                )))
+
+  (defun org-refile-to-datetree (&optional file)
+    "Refile a subtree to a datetree corresponding to it's timestamp.
+     The current time is used if the entry has no timestamp. If FILE
+     is nil, refile in the current file."
+    (interactive "f")
+    (let* ((datetree-date (or (org-entry-get nil "TIMESTAMP" t)
+                              (org-read-date t nil "now")))
+           (date (org-date-to-gregorian datetree-date))
+           )
+      (save-excursion
+        (with-current-buffer (current-buffer)
+          (org-cut-subtree)
+          (if file (find-file file))
+          (outline-show-all)
+          (org-datetree-find-date-create date)
+          (org-narrow-to-subtree)
+          (show-subtree)
+          (org-end-of-subtree t)
+          (goto-char (point-max))
+          (org-paste-subtree 4)
+          (widen)
+          ))
+      )
+    )
+  )
+
+
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-strict-missing-semi-warning nil)
+ '(org-agenda-files
+   (quote
+    ("/home/mischa/Dropbox/org/business.org" "/home/mischa/Dropbox/org/comp.org" "/home/mischa/Dropbox/org/comp_kb.org" "/home/mischa/Dropbox/org/dreikr.org" "/home/mischa/Dropbox/org/dreikr_kb.org" "/home/mischa/Dropbox/org/emacs_org_kb.org" "/home/mischa/Dropbox/org/flashklip.org" "/home/mischa/Dropbox/org/gamestoun.org" "/home/mischa/Dropbox/org/health.org" "/home/mischa/Dropbox/org/japanese.org" "/home/mischa/Dropbox/org/korean.org" "/home/mischa/Dropbox/org/linux.org" "/home/mischa/Dropbox/org/list.org" "/home/mischa/Dropbox/org/oojooman.org" "/home/mischa/Dropbox/org/org.org" "/home/mischa/Dropbox/org/private.org" "/home/mischa/Dropbox/org/programming.org" "/home/mischa/Dropbox/org/projects.org" "/home/mischa/Dropbox/org/refile.org" "/home/mischa/Dropbox/org/rocky.org" "/home/mischa/Dropbox/org/sysop.org" "/home/mischa/Dropbox/org/techshare.org")))
+ '(org-agenda-sorting-strategy
+   (quote
+    ((agenda todo-state-up time-up priority-down category-keep)
+     (todo priority-down category-keep)
+     (tags priority-down category-keep)
+     (search category-keep))))
+ '(org-capture-templates (quote (("j" "Journal" entry (file+datetree nil) "%U %?"))))
+ '(org-hide-emphasis-markers t)
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill)))
+ '(org-return-follows-link t)
+ '(org-time-stamp-custom-formats (quote ("<%m/%d/%y %a>" . "<%H:%M>")))
+ '(package-selected-packages
+   (quote
+    (csv-mode tern js2-mode web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js-doc coffee-mode web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode ob-elixir flycheck-mix alchemist company elixir-mode smeargle orgit org-projectile pcache org-present org org-pomodoro alert log4e gntp org-download mmm-mode markdown-toc markdown-mode magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit with-editor ws-butler window-numbering which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide ido-vertical-mode hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async quelpa package-build spacemacs-theme)))
+ '(powerline-text-scale-factor 1.0))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-date ((t (:foreground "#7590db" :height 1.0))))
+ '(org-agenda-date-today ((t (:inherit bold :foreground "#4f97d7" :height 1.0))))
+ '(org-agenda-done ((t (:foreground "#86dc2f" :height 1.0))))
+ '(org-document-title ((t (:inherit bold :foreground "#bc6ec5" :underline t :height 1.0))))
+ '(org-level-1 ((t (:inherit bold :foreground "#4f97d7" :height 1.0))))
+ '(org-level-2 ((t (:inherit nil :foreground "#2d9574" :height 1.0))))
+ '(org-level-3 ((t (:foreground "#67b11d" :weight normal :height 1.0))))
+ '(org-scheduled-today ((t (:foreground "#bc6ec5" :height 1.0))))
+ '(org-tag ((t (:weight light)))))
